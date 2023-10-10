334ac5569c4a94a391b093073233ed54f2913858
Your Namedamberghalley <you@example.comdamberghalley8@gmail.com>
2023-10-10 14:03:43 +0600

    game

---
+++ game/game.ino
@@ -0,0 +1,20 @@
+void setup(){}
+
+int multiplaction(int number, int increment){
+  int product = number * increment;
+  return product;
+}
+  
+int addition(int number,int increment){
+  int sum = (number*increment);
+  return sum;
+}
+
+void loop(){
+
+int four = multiplaction(2,2);
+
+int six = addition(3,3);
+
+
+}
